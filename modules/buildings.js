0005df5786e6c3fae6b168d8f974989a1bf5badc
David Pei <david.pei@dramafever.com>
2021-11-10 09:33:18 -0500

    fix a bug that food instead of gem was used to calculate the cost

--- modules/buildings.js
+++ modules/buildings.js
@@ -202,10 +202,13 @@ function buyBuildings() {
         safeBuyBuilding('Tribute');
     }
     //Nurseries
-    var warpstationCost = getBuildingItemPrice('Warpstation', "food", false, 1);
-    var nurseyCost = getBuildingItemPrice('Nursery', "food", false, 1);
-    var personalCostOverride = nurseyCost <= warpstationCost * 0.01;
-    if (game.buildings.Nursery.locked == 0 && (!hidebuild &&( personalCostOverride || game.global.world >= getPageSetting('NoNurseriesUntil') || getPageSetting('NoNurseriesUntil') < 1) && (getPageSetting('MaxNursery') > game.buildings.Nursery.owned || getPageSetting('MaxNursery') == -1)) || (game.global.challengeActive != "Daily" && getPageSetting('PreSpireNurseries') > game.buildings.Nursery.owned && isActiveSpireAT()) || (game.global.challengeActive == "Daily" && getPageSetting('dPreSpireNurseries') > game.buildings.Nursery.owned && disActiveSpireAT())) {
+    var warpstationCost = getBuildingItemPrice('Warpstation', "gem", false, 1);
+    console.log("warp cost", warpstationCost);
+    var nurseryCost = getBuildingItemPrice('Nursery', "gem", false, 1);
+    console.log("nursery cost", warpstationCost);
+    var personalBuyDecisionOverride = nurseryCost <= warpstationCost * 0.01;
+    console.log("should buy: ", personalBuyDecisionOverride);
+    if (game.buildings.Nursery.locked == 0 && (!hidebuild &&( personalBuyDecisionOverride || game.global.world >= getPageSetting('NoNurseriesUntil') || getPageSetting('NoNurseriesUntil') < 1) && (getPageSetting('MaxNursery') > game.buildings.Nursery.owned || getPageSetting('MaxNursery') == -1)) || (game.global.challengeActive != "Daily" && getPageSetting('PreSpireNurseries') > game.buildings.Nursery.owned && isActiveSpireAT()) || (game.global.challengeActive == "Daily" && getPageSetting('dPreSpireNurseries') > game.buildings.Nursery.owned && disActiveSpireAT())) {
 	safeBuyBuilding('Nursery');
     }
 
